461105b2aa5dde2e4d30bda9cd5b61f7030e395f
Antonio Gelameris <toniogela89@gmail.com>
2022-11-10 15:47:31 +0100

    Adding missing description to tenant seed

--- src/main/scala/it/pagopa/interop/tenantprocess/api/adapters/TenantManagementAdapters.scala
+++ src/main/scala/it/pagopa/interop/tenantprocess/api/adapters/TenantManagementAdapters.scala
@@ -44,7 +44,7 @@ object TenantManagementAdapters extends SprayJsonSupport with DefaultJsonProtoco
 
   implicit class DependencyMailWrapper(private val m: DependencyMail) extends AnyVal {
     def toApi: Mail                = Mail(kind = m.kind.toApi, address = m.address, createdAt = m.createdAt)
-    def toSeed: DependencyMailSeed = DependencyMailSeed(m.kind, m.address)
+    def toSeed: DependencyMailSeed = DependencyMailSeed(m.kind, m.address, m.description)
   }
 
   implicit class DependencyMailKindWrapper(private val k: DependencyMailKind) extends AnyVal {
