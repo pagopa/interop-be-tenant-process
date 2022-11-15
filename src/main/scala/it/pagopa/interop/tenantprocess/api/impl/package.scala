e224758b072d1981e9f0e5872fe1fe331d73e076
Roberto Pavia <bluesky14.rp@gmail.com>
2022-11-15 17:34:49 +0100

    Added name field to tenant

--- src/main/scala/it/pagopa/interop/tenantprocess/api/impl/package.scala
+++ src/main/scala/it/pagopa/interop/tenantprocess/api/impl/package.scala
@@ -46,7 +46,7 @@ package object impl extends SprayJsonSupport with DefaultJsonProtocol {
   implicit def tenantVerifierFormat: RootJsonFormat[TenantVerifier] = jsonFormat5(TenantVerifier)
   implicit def tenantRevokerFormat: RootJsonFormat[TenantRevoker]   = jsonFormat6(TenantRevoker)
 
-  implicit def tenantFormat: RootJsonFormat[Tenant]             = jsonFormat8(Tenant)
+  implicit def tenantFormat: RootJsonFormat[Tenant]             = jsonFormat9(Tenant)
   implicit def problemErrorFormat: RootJsonFormat[ProblemError] = jsonFormat2(ProblemError)
   implicit def problemFormat: RootJsonFormat[Problem]           = jsonFormat5(Problem)
 
