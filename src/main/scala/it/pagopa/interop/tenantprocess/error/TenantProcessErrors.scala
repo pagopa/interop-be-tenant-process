19792587b90e2d1203c5abf485584a0aefddf39c
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-12-06 18:25:03 +0100

    pin-2210: Remove unused errors

--- src/main/scala/it/pagopa/interop/tenantprocess/error/TenantProcessErrors.scala
+++ src/main/scala/it/pagopa/interop/tenantprocess/error/TenantProcessErrors.scala
@@ -14,66 +14,54 @@ object TenantProcessErrors {
         s"Conflict on Tenant SelfCare ID update for tenant $tenantId: old value $existingSelfcareId - new value $newSelfcareId"
       )
 
-  final case class CertifiedAttributeNotFound(origin: String, attributeCode: String)
-      extends ComponentError("0003", s"Attribute ($origin, $attributeCode) not found")
-
   final case class DeclaredAttributeNotFound(tenantId: UUID, attributeId: String)
-      extends ComponentError("0004", s"Attribute $attributeId not found for Tenant $tenantId")
-
-  final case class VerifiedAttributeNotFound(tenantId: UUID, attributeId: String)
-      extends ComponentError("0005", s"Attribute $attributeId not found for Tenant $tenantId")
-
-  final case class AttributeAlreadyVerified(targetTenantId: UUID, requesterTenantId: UUID, attributeId: UUID)
-      extends ComponentError(
-        "0006",
-        s"Attribute $attributeId has been already verified for $targetTenantId by $requesterTenantId"
-      )
+      extends ComponentError("0003", s"Attribute $attributeId not found for Tenant $tenantId")
 
   object VerifiedAttributeSelfVerification
-      extends ComponentError("0007", s"Organizations are not allowed to verify own attributes")
+      extends ComponentError("0004", s"Organizations are not allowed to verify own attributes")
 
   final case class AttributeVerificationNotAllowed(consumerId: UUID, attributeId: UUID)
       extends ComponentError(
-        "0008",
+        "0005",
         s"Organization is not allowed to verify attribute $attributeId for tenant $consumerId"
       )
 
   final case class AttributeAlreadyRevoked(targetTenantId: UUID, requesterTenantId: UUID, attributeId: UUID)
       extends ComponentError(
-        "0009",
+        "0006",
         s"Attribute $attributeId has been already revoked for $targetTenantId by $requesterTenantId"
       )
 
   object VerifiedAttributeSelfRevocation
-      extends ComponentError("0010", s"Organizations are not allowed to revoke own attributes")
+      extends ComponentError("0007", s"Organizations are not allowed to revoke own attributes")
 
   final case class AttributeRevocationNotAllowed(consumerId: UUID, attributeId: UUID)
       extends ComponentError(
-        "0011",
+        "0008",
         s"Organization is not allowed to revoke attribute $attributeId for tenant $consumerId"
       )
 
   final case class RegistryAttributeNotFound(origin: String, value: String)
-      extends ComponentError("0012", s"Attribute $origin/$value not found in registry")
+      extends ComponentError("0009", s"Attribute $origin/$value not found in registry")
 
   final case class TenantNotFound(origin: String, value: String)
-      extends ComponentError("0013", s"Tenant $origin/$value not found")
+      extends ComponentError("0010", s"Tenant $origin/$value not found")
 
-  final case class TenantByIdNotFound(tenantId: UUID) extends ComponentError("0014", s"Tenant $tenantId not found")
+  final case class TenantByIdNotFound(tenantId: UUID) extends ComponentError("0011", s"Tenant $tenantId not found")
 
   final case class CertifiedAttributeNotFoundInTenant(tenantId: UUID, attributeOrigin: String, attributeCode: String)
       extends ComponentError(
-        "0014",
+        "0012",
         s"Certified Attribute ($attributeOrigin, $attributeCode) not found in tenant $tenantId"
       )
 
   final case class DeclaredAttributeNotFoundInTenant(tenantId: UUID, attributeId: UUID)
-      extends ComponentError("0015", s"Declared Attribute $attributeId not found in tenant $tenantId")
+      extends ComponentError("0013", s"Declared Attribute $attributeId not found in tenant $tenantId")
 
   final case class VerifiedAttributeNotFoundInTenant(tenantId: UUID, attributeId: UUID)
-      extends ComponentError("0015", s"Verified Attribute $attributeId not found in tenant $tenantId")
+      extends ComponentError("0014", s"Verified Attribute $attributeId not found in tenant $tenantId")
 
   final case class TenantAttributeNotFound(tenantId: UUID, attributeId: UUID)
-      extends ComponentError("0016", s"Attribute $attributeId not found for Tenant $tenantId")
+      extends ComponentError("0015", s"Attribute $attributeId not found for Tenant $tenantId")
 
 }
